b91aa07834072e15d5301580fa34132c29988149
Mossati <xmossati9786@gmail.com>
2024-01-18 20:47:40 +0100

    code

--- script/script.js
+++ script/script.js
@@ -19,10 +19,82 @@ window.addEventListener("scroll", FactsList());
 
 // tijdlijn
 var timeList = document.querySelector(".timeline-list");
-var timeItem = timeList.getElementsByTagName('li');
-var ItemH2 = timeList.getElementsByTagName('h2');
-var ItemSpan = timeList.getElementsByTagName('span');
+var items = timeList.querySelectorAll('li');
+var timeBar = document.querySelector(".time-bar");
 
-ItemH2.addEventListener("click", function(){
-    ItemSpan[2];
+items.forEach(function(li) {
+    var itemBtn = li.querySelector('.btn-time');
+    var itemP = li.querySelector('p');
+    var itemImg = li.querySelector('img');
+    var itemTitle = li.querySelector('h2');
+    var itemBar = li.querySelector(".title-bar");
+
+    itemBtn.addEventListener('click', function () {
+        var itemPosition = li.offsetTop;
+        timeBar.style.height = (itemPosition / 16 + 1.75) + "em";
+
+        // button text
+        var btnIcon = itemBtn.querySelector('i');
+
+        if (itemP || itemImg) {
+            if (btnIcon.classList.contains("fa-chevron-down")) {
+                btnIcon.classList.remove("fa-chevron-down");
+                btnIcon.classList.add("fa-xmark");
+            }else {
+                btnIcon.classList.add("fa-chevron-down");
+                btnIcon.classList.remove("fa-xmark");
+            }
+        }
+
+        //titel bar animatie
+        if (itemBar) {
+            var barStyle = window.getComputedStyle(itemBar);
+
+            if (barStyle.display === "none") {
+                itemBar.style.display = "block";
+            } else {
+                itemBar.style.display = "none";
+            }
+
+            if (itemBar.classList.contains("keyframe-bar")) {
+                itemBar.classList.remove("keyframe-bar");
+            }else {
+                itemBar.classList.add("keyframe-bar");
+            }
+        }
+        
+        // paragraaf animatie
+        if (itemP) {
+            var pStyle = window.getComputedStyle(itemP);
+
+            if (pStyle.display === "none") {
+                itemP.style.display = "block";
+            } else {
+                itemP.style.display = "none";
+            }
+
+            if (itemP.classList.contains("keyframe-fade-slide")) {
+                itemP.classList.remove("keyframe-fade-slide");
+            }else {
+                itemP.classList.add("keyframe-fade-slide");
+            }
+        }
+
+        // afbeelding animatie
+        if (itemImg) {
+            var imgStyle = window.getComputedStyle(itemImg);
+
+            if (imgStyle.display === "none") {
+                itemImg.style.display = "block";
+            } else {
+                itemImg.style.display = "none";
+            }
+
+            if (itemImg.classList.contains("keyframe-fade-slide-img")) {
+                itemImg.classList.remove("keyframe-fade-slide-img");
+            }else {
+                itemImg.classList.add("keyframe-fade-slide-img");
+            }
+        }
+    });
 });
